01acbe9a15a38636a2cc705492ab6c14d4a62de8
Ashley Christie <achristie0319@gmail.com>
2021-10-18 22:56:42 -0400

    nav-bar and landing page start

--- src/Components/Landing Page/Landing Page/LandingPage.js
+++ src/Components/Landing Page/Landing Page/LandingPage.js
@@ -1,1 +1,23 @@
-﻿
+﻿import React from "react";
+import "./LandingPage.css";
+
+function LandingPage() {
+  return (
+    <div className="landing-app">
+      <div className="typewriter">
+        <p className="type_1">Hi, I'm Ashley Christie.</p>
+        <p className="type_2">I am a Front-End</p>
+        <p className="type_3">Developer.</p>
+      </div>
+      <div className="star-grid">
+        <div className="star-one"></div>
+        <div className="star-two"></div>
+        <div className="star-three"></div>
+        <div className="star-four"></div>
+        <div className="star-five"></div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
--- src/Components/Landing Page/NavBar/NavBar.js
+++ src/Components/Landing Page/NavBar/NavBar.js
@@ -1,1 +1,22 @@
-﻿
+﻿import React from "react";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <div className="nav-links">
+      <ul>
+        <li>
+          <a href="#">ABOUT</a>
+        </li>
+        <li>
+          <a href="#">WORK</a>
+        </li>
+        <li>
+          <a href="#">CONTACT</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
